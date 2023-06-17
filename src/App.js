95680abc8d572cf754a26635e9ba1118927ad43c
jazeela95 <jazeelat95@gmail.com>
2023-06-17 12:05:46 -0700

    se commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">kudumpashri</div>;
+  return (
+    <div className="App">
+      kudumpashri
+      <h2>lorem</h2>
+    </div>
+  );
 }
 
 export default App;
