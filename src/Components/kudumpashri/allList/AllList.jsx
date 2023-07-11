e1b3f7c51632afd4964b311db788cf1df56d556f
jazeela95 <jazeelat95@gmail.com>
2023-07-11 15:33:28 -0700

    update

--- src/Components/kudumpashri/allList/AllList.jsx
+++ src/Components/kudumpashri/allList/AllList.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { allListkudumpashriApi } from "../../../Store/api/kudumpashri";
 import { BsFillEyeFill } from "react-icons/bs";
-
+import "./allList.scss";
 const AllList = () => {
   const dispatch = useDispatch();
   const { tableData } = useSelector((state) => ({
